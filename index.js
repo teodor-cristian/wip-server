7d3ed2725f03e336e03e2dbd203600c048b21c90
Teodor Solca <t-c.solca@metro.digital>
2021-06-27 22:16:24 +0300

    Changed port no

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const cors = require('cors');
 
-const port = 5000;
+const port = 9090;
 
 app.use(cors());
 app.use(require('./routes/posts'))
 
 app.listen(port, () => {
-  console.log(`Server is on port 5000`);
+  console.log(`Server is on port 9090`);
 });
